9809ae07bdeed79ae978284a33762920ffceedf7
luisherrera <luisherrerapulido@gmail.com>
2023-11-07 03:34:39 -0500

    cuarto commiteereeeeeejrjr

--- empleado/settings.py
+++ empleado/settings.py
@@ -95,7 +95,11 @@ WSGI_APPLICATION = 'empleado.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config()
+    'default': dj_database_url.config(
+        # Feel free to alter this value to suit your needs.
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        conn_max_age=600
+    )
 }
 
 # Password validation
